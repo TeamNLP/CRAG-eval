e4998c20c5dd302d3f8904ff655bc5f8e6965bac
oneonlee <dg.lee@postech.ac.kr>
2024-06-26 09:27:15 +0000

    add gpt-4o-2024-05-13

--- scripts/evaluation.sh
+++ scripts/evaluation.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-cd ..
 
-target_model_list=( "meta-llama/Meta-Llama-3-8B-Instruct" "meta-llama/Meta-Llama-3-70B-Instruct" )
+evaluation_model_name="gpt-4o-2024-05-13" #`gpt-4o-2024-05-13`, `gpt-4-0125-preview`, or `gpt-3.5-turbo-0125`
 
-for target_model_name in ${target_model_list[@]}; do
-    python ./evaluation.py \
-        --dataset_path ./ms_marco.jsonl \
-        --evaluation_model_name gpt-3.5-turbo-0125 \
+for target_model_name in "meta-llama/Meta-Llama-3-8B-Instruct" "meta-llama/Meta-Llama-3-70B-Instruct"; do
+    python evaluation.py \
+        --dataset_path ms_marco.jsonl \
+        --evaluation_model_name ${evaluation_model_name} \
         --target_model_name ${target_model_name} \
-        --output_path ./result
+        --output_path result
 done
